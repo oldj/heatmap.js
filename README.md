e333a0f3f8d657cc8ace81f783e3fe439537c913
oldj <oldj.wu@gmail.com>
2015-11-12 00:26:56 +0800

    兼容 CommonJS 等模块引用。

--- README.md
+++ README.md
@@ -1,45 +1,43 @@
 heatmap.js
 ==========
 
-    @author: oldj
-    @blog: http://oldj.net
+* @author: oldj
+* @blog: http://oldj.net
 
-heatmap.js是一个基于Canvas的热图绘制库。
+heatmap.js is a heatmap library based on Canvas.
 
- 
-* 版本：1.0
-* 作者：oldj
+
 * demo：[http://oldj.github.io/heatmap.js/](http://oldj.github.io/heatmap.js/)
- 
-## 初始化组件
- 
+
+## Initialize
+
     var heatmap = new HeatMap(canvas_node, width, height);
 
-## API说明
- 
+## API
+
 * *addData(data)*
- 
-    添加数据。其中 data 是一个数组，形如：
- 
-    [[x1, y1, n1], [x2, y2, n2], [x3, y3, n3], ... ]
- 
-    含义为每个坐标以及它对应的权重，比如：
- 
-    [[12, 587, 1], [425, 220, 2], [449, 28, 6], ...]
- 
-    其中第三位 n 也可以省略，如果省略，则默认为 1，比如：
- 
-    [[144, 59, 1], [209, 94, 1], [83, 391, 1], ...]
- 
-    等价于：
- 
-    [[144, 59], [209, 94], [83, 391], ...]
+
+    add data(points), the parameter `data` is an Array looks like:
+
+        [[x1, y1, n1], [x2, y2, n2], [x3, y3, n3], ... ]
+
+    set points and count/weight, such as:
+
+        [[12, 587, 1], [425, 220, 2], [449, 28, 6], ...]
+
+    the 3rd parameter `n` is optional with a default value `1`. For example:
+
+        [[144, 59, 1], [209, 94, 1], [83, 391, 1], ...]
+
+    equals to:
+
+        [[144, 59], [209, 94], [83, 391], ...]
  
  
 * *render()*
  
-    绘制热图。
+    render the headmap.
  
 * *clear()*
  
-    清空画布。
+    clear the canvas.
--- demo/1.js
+++ demo/1.js
@@ -5,34 +5,33 @@
 
 
 window.onload = function () {
-	var w = 800, h = 600;
-	var hm = new HeatMap(document.getElementById("test-canvas"), w, h);
-	var rnd_points = [];
-	var init_count = 3;
-	var pos;
-	var i, l, j = 15, wv = w / 4;
+    var w = 800, h = 600;
+    var hm = new HeatMap(document.getElementById("test-canvas"), w, h);
+    var rnd_points = [];
+    var init_count = 3;
+    var pos;
+    var i, l, j = 15, wv = w / 4;
 
-	for (i = 0; i < init_count; i ++) {
-		rnd_points.push([
-			Math.floor(Math.random() * w),
-			Math.floor(Math.random() * h),
-			Math.floor(Math.random() * 10) + 1
-		]);
-	}
+    for (i = 0; i < init_count; i++) {
+        rnd_points.push([
+            Math.floor(Math.random() * w),
+            Math.floor(Math.random() * h),
+            Math.floor(Math.random() * 10) + 1
+        ]);
+    }
 
-	while(j--) {
-		l = rnd_points.length;
-		while (l--) {
-			pos = rnd_points[l];
-			rnd_points.push([
-				Math.floor((Math.random() - 0.5) * wv + pos[0]),
-				Math.floor((Math.random() - 0.5) * wv + pos[1]),
-				Math.floor(Math.random() * 10) + 1
-			]);
-		}
-	}
+    while (j--) {
+        l = rnd_points.length;
+        while (l--) {
+            pos = rnd_points[l];
+            rnd_points.push([
+                Math.floor((Math.random() - 0.5) * wv + pos[0]),
+                Math.floor((Math.random() - 0.5) * wv + pos[1]),
+                Math.floor(Math.random() * 10) + 1
+            ]);
+        }
+    }
 
-	hm.addData(rnd_points);
-
-	hm.render();
+    hm.addData(rnd_points);
+    hm.render();
 };
